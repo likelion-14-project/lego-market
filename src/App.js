d80c71c41fe0d7a6be13c5308c67b985a2bb6053
wonhyeong lee <bigfile57@naver.com>
2022-07-05 09:58:47 +0900

    📝  폴더구조 정리 , Readme 수정

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+import Test from "./test/test";
+
+function App() {
+
+  return (
+          <BrowserRouter>
+          <GlobalStyle />
+            <Routes>
+              <Route path="/" element={<Test />}/>
+            </Routes>
+          </BrowserRouter>
+          );
+}
+
+export default App;
