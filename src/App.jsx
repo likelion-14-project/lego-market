7119f12a41fe9ae8e2326650a4b1406fdfdecf6c
LeeHyeonSeop <gustjq4146@naver.com>
2022-07-08 15:22:37 +0900

    글로버 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import reset from 'styled-reset'
+import {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+`
+
 function App() {
   return (
     <div className="App">
-      <h1>test2</h1>
+      <GlobalStyle />
     </div>
   );
 }
