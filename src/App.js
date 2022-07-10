fa507b300539455adec57aac34d09f63610c6858
leehyeonseop <gustjq4146@naver.com>
2022-07-10 15:20:43 +0900

    글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+`;
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+    return (
+        <div className='App'>
+            <GlobalStyle />
+        </div>
+    );
 }
 
 export default App;
