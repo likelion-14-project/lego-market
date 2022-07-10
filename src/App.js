8ca1467d09f49456f6016becf4f8a9f6732204b9
leehyeonseop <gustjq4146@naver.com>
2022-07-10 15:43:42 +0900

    글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import Splash from "./pages/Splash";
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+`
 
 function App() {
     return (
