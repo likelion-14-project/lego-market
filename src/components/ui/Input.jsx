8c2e5b2a3189ae01d8e66e763a9a21dce1979699
leehyeonseop <gustjq4146@naver.com>
2022-07-10 16:08:55 +0900

    인풋 컴포넌트 수정

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { useRef } from 'react'
+import {useForm} from 'react-hook-form'
 
+
+const Wrapper = styled.div`
+    margin-top: ${props => props.marginTop}px;
+`
 
 const InputLabel = styled.label`
     font-family: 'Spoqa Han Sans Neo';
@@ -14,22 +17,12 @@ const InputLabel = styled.label`
     
 `
 
-const InputData = styled.input.attrs({
-    required : true
-})`
-display:block;
+const InputData = styled.input`
     border: none;
     border-bottom: 1px solid #DBDBDB;
     padding-bottom: 8px;
     width: 100%;
     box-sizing: border-box;
-    &:focus-within {
-    display: block;
-    width: 100%;
-    outline: none;
-    border: none;
-    border-bottom: solid 1px #f26e22;
-  }
     ::placeholder {
         font-family: 'Spoqa Han Sans Neo';
         font-style: normal;
@@ -37,24 +30,48 @@ display:block;
         font-size: 14px;
         color: #DBDBDB;
     }
-    margin-bottom: ${(props) => (props.marginBottom)}px;
+    
+    &:focus-within {
+    width: 100%;
+    outline: none;
+    border: none;
+    border-bottom: solid 1px #f26e22;
+    
+    }
+`
+
+const ErrorMessage = styled.p`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #EB5757;
 `
 
 
 function Input(props) {
 
-    const {label, marginBottom, placeholder, onChange, type, name, id, onKeyUp} = props
-    const inputRef = useRef()
-
+    const {label, placeholder, type, name, className, marginTop, register, condition, errors} = props
     
     return (
-        <>
+        <Wrapper className={className} marginTop={marginTop}>
             <InputLabel>{label}</InputLabel>
             <InputData
-                {...props}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                {...register(name,condition)}
             />
-        </>
+            {errors[name] && errors[name].type &&<ErrorMessage>{errors[name].message}</ErrorMessage>}
+        </Wrapper>
     )
+}
+
+Input.defaultProps = {
+    errors : {},
+    name : '',
+    register : () => {},
+    condition : {}
 }
 
 export default Input
