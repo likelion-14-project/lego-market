3886cd338ef0d1ebf341f17a7727dbadf7170c00
Jamgoori <lks2354@naver.com>
2022-07-11 21:45:13 +0900

    :truck: components에 본파일 이동후 pages에 깔끔파일에 import

---
+++ src/pages/AddProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddProduct from '../components/addproduct/AddProduct'
+function AddProductPage() {
+  return (
+    <>
+    <AddProduct/>
+    </>
+  )
+}
+
+export default AddProductPage
