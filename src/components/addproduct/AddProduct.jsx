d7936e454af97a1c873348cb5d7e94d63c13cc0b
박잼 <98381294+Jamgoori@users.noreply.github.com>
2022-07-13 16:19:41 +0900

    :fire: 실험코드 추가된거 삭제..

--- src/components/addproduct/AddProduct.jsx
+++ src/components/addproduct/AddProduct.jsx
@@ -46,25 +46,6 @@ const LabelDiv = styled.div`
   margin-bottom: 10px;
   display: block;
 `;
-function getNumber(obj){
-    
-  var num01;
-  var num02;
-  num01 = obj.value;
-  num02 = num01.replace(/\D/g,""); //숫자가 아닌것을 제거, 
-                                   //즉 [0-9]를 제외한 문자 제거; /[^0-9]/g 와 같은 표현
-  num01 = setComma(num02); //콤마 찍기
-  obj.value =  num01;
-
-}
-function setComma(n) {
-  var reg = /(^[+-]?\d+)(\d{3})/;   // 정규식
-  n += '';                          // 숫자를 문자열로 변환         
-  while (reg.test(n)) {
-     n = n.replace(reg, '$1' + ',' + '$2');
-  }         
-  return n;
-}
 
 const AddProduct = (props) => {
   const {
@@ -169,8 +150,6 @@ const AddProduct = (props) => {
             })}
             errors={errors}
             WarningMessage={WarningMessage}
-            onChange="getNumber(this);"
-            onKeyUp="getNumber(this);"
           />
         </div>
         <div>
