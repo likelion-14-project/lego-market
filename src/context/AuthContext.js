aee0b230f9f19a3f1a440e03b65acc9f9fff4347
LeeHyeonSeop <gustjq4146@naver.com>
2022-07-14 11:50:14 +0900

    :lipstick: AuthContext 추가 및 Login 훅 생성

---
+++ src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'login' :
+            return {...state, user : action.payload}
+        default :
+            return state
+    }
+}
+
+const AuthContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user : null
+    })
+
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export {AuthContext, authReducer, AuthContextProvider}
