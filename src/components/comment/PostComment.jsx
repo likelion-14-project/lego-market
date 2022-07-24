a093a6e12dfda6f05a6920b932e969f2be25bfdb
Hyebin-Woo <dev.hyebin@gmail.com>
2022-07-24 23:59:46 +0900

    :sparkles: 디테일페이지와 댓글컴포넌트 연결

--- src/components/comment/PostComment.jsx
+++ src/components/comment/PostComment.jsx
@@ -4,23 +4,25 @@ import { useEffect } from "react";
 import { useState } from "react";
 import InputFooter from "../ui/InputFooter";
 import CommentCard from "./CommentCard";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const CommentSection = styled.section`
     max-width: 390px;
     padding: 20px 16px 0;
-    /* margin : 0 auto; */
+    margin: 0 auto;
     // 임시로 많아지면 스크롤로 내려서 보게끔 했습니다.
-    margin: 300px auto 0;
+    /* margin: 300px auto 0;
     overflow-y: scroll;
-    height: 350px;
+    height: 350px; */
 `;
 
-function PostComment() {
+function PostComment({ post_id }) {
+    const { user } = useAuthContext();
     // 사용자가 입력하고 있는 댓글
     let [comment, setComment] = useState("");
     // 댓글리스트를 담기
     let [feedComments, setFeedComments] = useState([]);
-    const [disabled, setDisabled] = useState(false);
+    const [disabled, setDisabled] = useState(true);
 
     // 게시 클릭시 할생하는 post 함수
     let post = (e) => {
@@ -38,7 +40,7 @@ function PostComment() {
         };
 
         const res = await fetch(
-            "https://mandarin.api.weniv.co.kr/post/62cea75282fdcc712f43dfdd/comments/",
+            `https://mandarin.api.weniv.co.kr/post/${post_id}/comments/`,
             {
                 method: "POST",
 
@@ -56,15 +58,10 @@ function PostComment() {
     }
 
     async function getComments() {
-        localStorage.setItem(
-            "token",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzk5OWIyODJmZGNjNzEyZjQzN2ExZiIsImV4cCI6MTY2Mjk4MjAxNywiaWF0IjoxNjU3Nzk4MDE3fQ.1d70EV8trpy1Bn9ehIBAUS3cTPGoKMxoEd64OJIiDMo"
-        );
-
         const token = localStorage.getItem("token");
 
         const res = await fetch(
-            "https://mandarin.api.weniv.co.kr/post/62cea75282fdcc712f43dfdd/comments/",
+            `https://mandarin.api.weniv.co.kr/post/${post_id}/comments/`,
             {
                 method: "GET",
 
@@ -89,20 +86,23 @@ function PostComment() {
         <>
             <CommentSection>
                 <ul>
-                    {feedComments.map((commentsArr, i) => {
-                        return (
-                            <CommentCard
-                                userProfile={commentsArr.author.image}
-                                userName={commentsArr.author.username}
-                                userCommet={commentsArr.content}
-                                key={i}
-                            />
-                        );
-                    })}
+                    {feedComments
+                        .map((commentsArr, i) => {
+                            return (
+                                <CommentCard
+                                    userProfile={commentsArr.author.image}
+                                    userName={commentsArr.author.username}
+                                    userCommet={commentsArr.content}
+                                    createdAt={commentsArr.createdAt}
+                                    key={i}
+                                />
+                            );
+                        })
+                        .reverse()}
                 </ul>
             </CommentSection>
             <InputFooter
-                // img={.author.image}
+                img={user.image}
                 ir="댓글입력하기"
                 placeholder="댓글 입력하기"
                 value={comment}
