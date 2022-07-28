696b8e1bd2a2d1ab746fd2867fce35db51c43e8d
Hyebin-Woo <dev.hyebin@gmail.com>
2022-07-29 01:48:10 +0900

    :bento: 레고이미지파일로 교체

--- src/components/home/HomeMain.jsx
+++ src/components/home/HomeMain.jsx
@@ -1,43 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 const HomeMainWrap = styled.div`
-  position : absolute;
-  left :50%;
-  top : 50%;
-  transform: translate(-50%,-50%);
-  text-align : center;
-  img {
-    width: 100px;
-    height: 100px;
-  }
-  .text-gray {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 14px;
-    margin: 23px 0 20px 0;
-    color: #767676;
-  }
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    img {
+        width: 100px;
+        height: 100px;
+    }
+    .text-gray {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 14px;
+        margin: 23px 0 20px 0;
+        color: #767676;
+    }
 
-  button.btn-search {
-    width: 120px;
-    height: 44px;
-    background-color: #fe6e22;
-    color: white;
-    border-radius: 44px;
-  }
+    button.btn-search {
+        width: 120px;
+        height: 44px;
+        background-color: #ea4335;
+        color: white;
+        border-radius: 44px;
+    }
 `;
 
 function HomeMain() {
-    const logo = "icons/symbol-logo-gray.png"
-  return (
-      <HomeMainWrap>
-        <img src={logo} alt="기본로고" />
-        <p className="text-gray">유저를 검색해 팔로우 해보세요 !</p>
-        <button type="button" className="btn-search">
-          검색하기
-        </button>
-      </HomeMainWrap>
-  );
+    const logo = "icons/symbol-logo-gray.png";
+    return (
+        <HomeMainWrap>
+            <img src={logo} alt="기본로고" />
+            <p className="text-gray">유저를 검색해 팔로우 해보세요 !</p>
+            <button type="button" className="btn-search">
+                검색하기
+            </button>
+        </HomeMainWrap>
+    );
 }
 
 export default HomeMain;
